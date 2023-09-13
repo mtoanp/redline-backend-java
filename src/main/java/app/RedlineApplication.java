ba2040a1a2862d1d1ab90fbeba93700994408a14
mtoan <mto.work@outlook.com>
2023-09-13 17:14:22 +0200

    CrossOrigin disable

--- src/main/java/app/RedlineApplication.java
+++ src/main/java/app/RedlineApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 //@SpringBootApplication
+//@CrossOrigin(maxAge = 3600)
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class RedlineApplication implements CommandLineRunner {
 
--- src/main/java/app/controller/ThemeController.java
+++ src/main/java/app/controller/ThemeController.java
@@ -5,21 +5,14 @@ import java.util.List;
 import javax.management.RuntimeErrorException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import app.model.entity.Theme;
 import app.model.validation.ThemeValidation;
 import app.service.FormationService;
 import app.service.ThemeService;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 
 @RequestMapping("api/themes")
